7341972484d28888faa0db5ceab5be649b7fcd4a
Dheemanth <dheemanth.vadde@sagarsoft.in>
2026-01-04 15:44:09 +0530

    route change

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { applyInterceptors } from './interceptor';
 
-const JOBS_BASE_URL='https://dev.bobjava.sentrifugo.com:8443/dev-candidate-app/api/v1/candidate'
-const RAZOR_BASE_URL='https://dev.bobjava.sentrifugo.com:8443/dev-candidate-app/api/v1/razorpay';
-const MASTER_BASE_URLs='https://dev.bobjava.sentrifugo.com:8443/dev-master-app/api/v1/master';
-const CANDIDATE_BASE_URL = "https://dev.bobjava.sentrifugo.com:8443/dev-candidate-app/api/v1/candidate";
+const JOBS_BASE_URL='https://bobjava.sentrifugo.com:8443/dev-candidate-app/api/v1/candidate'
+const RAZOR_BASE_URL='https://bobjava.sentrifugo.com:8443/dev-candidate-app/api/v1/razorpay';
+const MASTER_BASE_URLs='https://bobjava.sentrifugo.com:8443/dev-master-app/api/v1/master';
+const CANDIDATE_BASE_URL = "https://bobjava.sentrifugo.com:8443/dev-candidate-app/api/v1/candidate";
 
 const jobsapi = axios.create({
   baseURL: JOBS_BASE_URL,
